e8094e8071e1f016d88b0398bce0aba5fe3c357d
srijitcnair <srijitcnair@hotmail.com>
2022-01-02 10:16:12 -0500

    added shop page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import './App.css';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import SellPage from './pages/sell/sell.component'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div >
 
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
+          <Route exact path='/' component={ShopPage} />
+          <Route exact path='/shop' component={ShopPage} />
           <Route exact path='/sell' component={SellPage} />
         </Switch>
 
